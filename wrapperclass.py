2223c6e9c7b48761b74d02e68ea910316759ec8f
muresaijaideepreddy <saijaideepreddymure@tamu.edu>
2025-02-02 16:47:57 -0600

    Wrapper class for the extra logic or like a preprocessing

---
+++ wrapperclass.py
@@ -0,0 +1,24 @@
+import random 
+import gymnasium as gym
+class RandomActionWrapper(gym.ActionWrapper):
+    def __init__(self,env,epsilon =0.1):
+        super(RandomActionWrapper,self).__init__(env)
+        self.epsilon=epsilon
+    def action(self,action):
+        if random.random() < self.epsilon:
+            print("Random")
+            return self.env.action_space.sample()
+        return action
+    
+
+
+if __name__=="__main__":
+    env= RandomActionWrapper(gym.make("CartPole-v0"))
+    obs = env.reset()
+    total_reward=0.0
+    while True:
+        obs,reward,done,_,_= env.step(0)
+        total_reward += reward
+        if done:
+            break
+    print(f"Reward got {total_reward}")
